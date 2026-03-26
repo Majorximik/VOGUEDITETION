fb73dbd4cf8245b08df3aa244faac041e1960ba0
Majorximik <klemradsh@gmail.com>
2026-03-26 14:53:52 +0300

    Complete Dart tutorial lesson 6

--- lr1/dartpedia/cli/bin/cli.dart
+++ lr1/dartpedia/cli/bin/cli.dart
@@ -3,6 +3,16 @@ import 'package:command_runner/command_runner.dart';
 const version = '0.0.1';
 
 Future<void> main(List<String> arguments) async {
-  final commandRunner = CommandRunner()..addCommand(HelpCommand());
+  final commandRunner = CommandRunner(
+    onError: (Object error) {
+      if (error is Error) {
+        throw error;
+      }
+      if (error is Exception) {
+        print(error);
+      }
+    },
+  )..addCommand(HelpCommand());
+
   await commandRunner.run(arguments);
 }
